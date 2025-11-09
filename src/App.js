958acf690e07c6d53a7e6cd4fa8e4070cca9d57d
taeyeon0911 <sta0911@kau.kr>
2025-11-10 00:24:01 +0900

    20251110

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+// src/App.js
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+
 import Home from './pages/Home/Home';
 import Mypage from './pages/Mypage/Mypage';
 import Diffuser from './pages/ProductPage/Diffuser';
 import Perfume from './pages/ProductPage/Perfume';
-import Footer from './components/Footer';
-import Header from './components/Header';
 import New from './pages/ProductPage/New';
-import ToolBar from "./components/ToolBar";
- // 수정: Aboutus → AboutUs
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ToolBar from './components/ToolBar';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
-    <>
+    <CookiesProvider>
       <Header />
-      <ToolBar />
-      
+      <ToolBar isLogin={isLogin} onLoginChange={setIsLogin} />
+
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home onLoginChange={setIsLogin} />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/new" element={<New />} />
         <Route path="/perfume" element={<Perfume />} />
         <Route path="/diffuser" element={<Diffuser />} />
       </Routes>
-    
+
       <Footer />
-    </>
+    </CookiesProvider>
   );
 }
 
--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/ToolBar.css";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 const MoveToTop = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
@@ -9,13 +11,51 @@ const MoveToBottom = () => {
   window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
 };
 
-const ToolBar = () => {
+
+
+const handleLoginRedirect = () => {
+  const redirect_url =
+    process.env.NODE_ENV === "development"
+    ?"http://localhost:3000"
+    :"https://seolshop.netlify.app/";
+
+    const oauthUrl =
+    "http://sajang-dev-env-1.eba-qyu26pwn.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao"+
+    `?redirect_uri=${encodeURIComponent(redirect_url)}`;
+
+   
+    window.location.href = oauthUrl;
+};
+
+const ToolBar = ({ isLogin, onLoginChange }) => {
+  const [cookies, removeCookie] = useCookies(["accessToken"]);
+
+  const handleLogout = () => {
+    axios
+      .delete("/users/logout",{
+        headers:{
+          accept:"*/*",
+          Authorization: `Bearer ${cookies.accessToken}`,
+        },
+      })
+      .then(()=> {})
+      .catch((err)=>{
+        console.log("LOGOUT API 요청 실패",err);
+      });
+  };
+
   return (
     <div className="toolbar-container">
       <img
-        src={`${process.env.PUBLIC_URL}/icon/icon_login.svg`}
+        src={
+          isLogin
+          ? `${process.env.PUBLIC_URL}/icon/icon_logout.svg`
+          :`${process.env.PUBLIC_URL}/icon/icon_login.svg`
+        }
+          
         alt="login"
         className="toolbar-icon"
+        onClick={isLogin ? handleLogout : handleLoginRedirect}
       ></img>
       <img
         src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
