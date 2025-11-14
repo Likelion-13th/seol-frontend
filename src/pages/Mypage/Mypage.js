40650bbb41e8f42584418f59dee1d85253214cd4
taeyeon0911 <sta0911@kau.kr>
2025-11-14 13:21:14 +0900

    20251114(3)

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -32,32 +32,34 @@ const Mypage = () => {
                 console.log("API 요청 실패", err);
                 });
             }, [cookies.accessToken]);
-    const handleSave = async (zipcode,address,addressDetail) => {{
-        try{
-            const response = await axios.post("/users/address",
-                {
-                    "zipcode":zipcode,
-                    "address":address,
-                    "addressDetail":addressDetail
-                },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${cookies.accessToken}`,
-                    },
-                }
-            );
+const handleSave = async (zipcode, address, addressDetail) => {
+  try {
+    const response = await axios.post(
+      "/users/address",
+      {
+        zipcode,
+        address,
+        addressDetail,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookies.accessToken}`,
+        },
+      }
+    );
 
-            if(response.data.isSuccess){
-                alert("주소가 성공적으로 저장되었습니다.");
-            } else{
-                alert(`주소 저장 실패: ${response.data.message}`);
-            }
-        } catch (error){
-            console.error("주소 저장 오류:", error);
-            alert("주소 저장 중 오류가 발생했습니다.");
-        }
-    }};
+    if (response.data.isSuccess) {
+      alert("주소가 성공적으로 저장되었습니다.");
+    } else {
+      alert(`주소 저장 실패: ${response.data.message}`);
+    }
+  } catch (error) {
+    console.error("주소 저장 오류:", error);
+    alert("주소 저장 중 오류가 발생했습니다.");
+  }
+};
+
     return(
         <div className="page-container">
          <Profile profileData={profileData} />
